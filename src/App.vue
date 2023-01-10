00f42c222447df456371fabb4d51313226ef5efe
Арсен Меркин <a.merkin@MacBook-Air-Arsen.local>
2023-01-10 12:53:02 +0500

    some education ts test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <p>hello, job-finder!</p>
+  <p>{{ name }}</p>
+  <button @click="changeName('zelda')"></button>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string,
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    }
+  }
+});
+</script>
